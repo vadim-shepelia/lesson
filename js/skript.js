ae8a54adf35f813427e4a6a0740140b6eb3dc75a
Vadim <shepelia.vadim@gmail.com>
2021-08-30 09:10:48 +0300

    homeWork add object

--- js/skript.js
+++ js/skript.js
@@ -1,12 +1,20 @@
-alert(1);
-console.log('vadim');
 
-let user = {
-    name: "Вася"
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?", "");
+const personalMovieDB =
+  {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
   };
-  
-  let id = Symbol("id");
-  
-  user[id] = 1;
-  
-console.log( user);
+let lastFilm = prompt("Один из последних просмотреных фильмов?", "");
+let HowLike = +prompt("Насколько оценете его ?", "1-5") ;
+let lastFilm1 = prompt("Один из последних просмотреных фильмов?", "");
+let HowLike1 = +prompt("Насколько оценете его ?", "1-5") ;
+
+//не дбавлять данніе в обьект через точку , может призвести к багам 
+personalMovieDB.movies[lastFilm] = HowLike; 
+personalMovieDB.movies[lastFilm1] = HowLike1;
+
+console.log(personalMovieDB);
