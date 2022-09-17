20f25be94707eeb274a39828f319a8caa9a7f67c
qabot <qabot@qabot.local>
2022-09-17 10:07:35 +0300

    commit 1.1less

--- js/LessMaybes/homework-01.js
+++ js/LessMaybes/homework-01.js
@@ -17,39 +17,117 @@ function orderedTotal(total, ordered) {
 
 //Задание 3
 const ADMIN_PASSWORD = "jqueryismyjam";
-// let message = prompt("укажи пароль ", "");
-// if (message === null) {
-//   alert("Отменено пользователем!");
-//   alert("message = " + message );
-// } else if (message === ADMIN_PASSWORD) {
-//   alert("Добро пожаловать!");
-//   alert("message = " + message );
-// } else {
-//   alert("Доступ запрещен, неверный пароль!");
-//   alert("message = " + message );
-// }
+let message = prompt("укажи пароль ", "");
+if (message === null) {
+  alert("Отменено пользователем!");
+  alert("message = " + message );
+} else if (message === ADMIN_PASSWORD) {
+  alert("Добро пожаловать!");
+  alert("message = " + message );
+} else {
+  alert("Доступ запрещен, неверный пароль!");
+  alert("message = " + message );
+}
 
 //Задание 4
 let credits = 23580;
 let pricePerDroid = 3000;
-let totalPrice;
+let totalPrices;
 let droids = prompt("Скольки дроідів ви хочете купити ? ", 1);
 console.log(typeof droids);
 
 if (droids === null) {
   alert("Отменено пользователем!");
 } else if (typeof droids == "number") {
-  totalPrice = pricePerDroid * droids;
-  if (totalPrice > credits) {
+  totalPrices = pricePerDroid * droids;
+  if (totalPrices > credits) {
     console.log("Недостаточно средств на счету!");
   } else
     console.log(
       "Вы купили " +
         droids +
         " дроидов, на счету осталось " +
-        (credits - totalPrice) +
+        (credits - totalPrices) +
         " кредитов."
     );
 }
 
 //Задание 5
+let country = prompt("ввести имя страны", "").toLocaleLowerCase();
+let arrCountryCredit = {
+  Китай: 100,
+  Чили: 250,
+  Австралия: 170,
+  Индия: 80,
+  Ямайка: 120,
+};
+switch (country) {
+  case "китай":
+    console.log(
+      "Доставка в " +
+        country +
+        " будет стоить " +
+        `${arrCountryCredit.Китай}` +
+        " кредитов"
+    );
+    break;
+  case "чили":
+    console.log(
+      "Доставка в " +
+        country +
+        " будет стоить " +
+        `${arrCountryCredit.Чили}` +
+        " кредитов"
+    );
+    break;
+  case "австралия":
+    console.log(
+      "Доставка в " +
+        country +
+        " будет стоить " +
+        `${arrCountryCredit.Австралия}` +
+        " кредитов"
+    );
+    break;
+  case "индия":
+    console.log(
+      "Доставка в " +
+        country +
+        " будет стоить " +
+        `${arrCountryCredit.Индия}` +
+        " кредитов"
+    );
+    break;
+  case "ямайка":
+    console.log(
+      "Доставка в " +
+        country +
+        " будет стоить " +
+        `${arrCountryCredit.Ямайка}` +
+        " кредитов"
+    );
+    break;
+  default:
+    alert('В вашей стране доставка не доступна')
+}
+
+//Задание 6
+let totalPrice = 0;
+for (let i = 1; i <= 2; i++) {
+  let question = prompt("ввести число");
+  let foNumber = Number(question);
+  if (question === null) {
+    console.log('Общая сумма чисел равна '+ totalPrice );
+    break
+  } else if(isNaN(foNumber)) {
+    foNumber = 0;
+    totalPrice += foNumber;
+    console.log(totalPrice);
+    --i;
+  }else{
+    totalPrice += foNumber;
+    console.log(totalPrice);
+    --i;
+  }
+}
+
